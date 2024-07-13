97aed59019e12a6a0ed940c0a0735953b3c08b80
criztiandev <criztiandev@gmail.com>
2024-07-13 19:04:58 +0800

    Added requiremnts in auth controller

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,5 +4,8 @@ const router = Router();
 
 router.post("/login", authController.login);
 router.post("/register", authController.register);
+router.post("/forgot-password", authController.forgotPassword);
+router.post("/checkpoint/:id", authController.verifyAccount);
+router.put("/checkpoint/change-password/:id", authController.changePassword);
 
 export default router;
