0234707b259b359f564a5a75a1968ef28eaffe37
criztiandev <criztiandev@gmail.com>
2025-01-18 09:46:21 +0800

    Update TypeScript configuration and environment type definitions
    - Modified tsconfig.json to explicitly include node types for TypeScript compilation.
    - Updated the ProcessEnv interface in env.d.ts to change the PORT type from number to string for consistency with environment variable handling.

--- @types/env/env.d.ts
+++ @types/env/env.d.ts
@@ -1,15 +1,13 @@
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      NODE_ENV: "development" | "production";
-      PORT: number;
-      JWT_SECRET: string;
-      SESSION_SECRET: string;
-      COOKIE_SECRET: string;
-      ENCRYPTION_KEY_32: string;
-      ENCRYPTION_KEY_16: string;
-      MONGO_URI: string;
-    }
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "development" | "production";
+    PORT: string;
+    JWT_SECRET: string;
+    SESSION_SECRET: string;
+    COOKIE_SECRET: string;
+    ENCRYPTION_KEY_32: string;
+    ENCRYPTION_KEY_16: string;
+    MONGO_URI: string;
   }
 }
 
